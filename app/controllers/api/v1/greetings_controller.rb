f3955beaf02ec68da883a1b8bb11157002042b1e
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-10-13 19:24:48 +0200

    Fix Linter errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,5 +2,5 @@ class Api::V1::GreetingsController < ApplicationController
   def index
     @greeting = Message.order('RANDOM()').first
     render json: { greeting: @greeting.text }
-  end 
+  end
 end
