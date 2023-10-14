b78b8e3e551d5d5d12bff2159166bdfc897ab4e7
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-10-14 05:46:28 +0200

    Modifies rails to work with CORS

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module HelloRailsBackEnd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.ignore_favicon = true
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -19,9 +20,22 @@ module HelloRailsBackEnd
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Only loads a smaller set of middleware suitable for API only apps.
+    # Only loads a smaller set of middleware suitable for API-only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # CORS Configuration
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:5173' # Replace with your frontend application's origin
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
+      end
+    end
   end
 end
+
